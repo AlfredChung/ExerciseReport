8bd126f11692ebedfb677d0c106c87a2cad60b96
AlfredChung <1258475623@qq.com>
2015-10-13 22:55:06 +0800

    delete all

--- 实验2/魏其南/net/wqn/model/ReadWriteDAO.java
+++
@@ -1,9 +0,0 @@
-package net.wqn.model;
-
-import java.util.ArrayList;
-import net.wqn.domian.Employee;
-
-public interface ReadWriteDAO {  //½Ó¿Ú
-	ArrayList<Employee> read();	
-	void Write(String context);
-}
